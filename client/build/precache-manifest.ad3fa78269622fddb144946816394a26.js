9c17ad6a3c4088c760aedd0bd7f41afe8308b3e9
NoneNone1999 <daniel.masorete@gmail.com>
2020-04-11 21:17:01 +0300

    make it better

---
+++ client/build/precache-manifest.ad3fa78269622fddb144946816394a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738c2358833d0c2ae04721687acbb521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ee91649674540eba0e",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "965a1d6a8cba1ee63782",
+    "url": "/static/css/main.b5cb710d.chunk.css"
+  },
+  {
+    "revision": "b2ee91649674540eba0e",
+    "url": "/static/js/2.4c3e0eed.chunk.js"
+  },
+  {
+    "revision": "225cdc140a05e8d6d471b53b3cfcae51",
+    "url": "/static/js/2.4c3e0eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965a1d6a8cba1ee63782",
+    "url": "/static/js/main.b3de7390.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
